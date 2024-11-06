32d790f5d53ccae1be75711d481184bc29511f5f
yamageji <koyama.shigehito.chabatake102@gmail.com>
2024-11-06 22:39:14 +0900

    fix: fix style sample page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,80 @@
 <script lang="ts">
-  import UseClipboardItems from '../core/useClipboardItems/demo.svelte';
-  import UseClipboard from '../core/useClipboard/demo.svelte';
-  import UsePermission from '../core/usePermission/demo.svelte';
-  import UseTimeoutFn from '../shared/useTimeoutFn/demo.svelte';
-  import UseBrowserLocation from '../core/useBrowserLocation/demo.svelte';
+  // State
+  import UseLocalStorage from '../core/useLocalStorage/demo.svelte';
+  import UseSessionStorage from '../core/useSessionStorage/demo.svelte';
+  import UseStorage from '../core/useStorage/demo.svelte';
+  // Browser
   import UseBreakpoints from '../core/useBreakpoints/demo.svelte';
+  import UseBrowserLocation from '../core/useBrowserLocation/demo.svelte';
+  import UseClipboard from '../core/useClipboard/demo.svelte';
+  import UseClipboardItems from '../core/useClipboardItems/demo.svelte';
+  import UseColorMode from '../core/useColorMode/demo.svelte';
+  import UseDark from '../core/useDark/demo.svelte';
+  import UseEventListener from '../core/useEventListener/demo.svelte';
   import UseEyeDropper from '../core/useEyeDropper/demo.svelte';
   import UseFavicon from '../core/useFavicon/demo.svelte';
-  import UseDark from '../core/useDark/demo.svelte';
-  import UseColorMode from '../core/useColorMode/demo.svelte';
-  import UseMounted from '../core/useMounted/demo.svelte';
   import UseMediaQuery from '../core/useMediaQuery/demo.svelte';
+  import UsePermission from '../core/usePermission/demo.svelte';
   import UsePreferredColorScheme from '../core/usePreferredColorScheme/demo.svelte';
   import UsePreferredContrast from '../core/usePreferredContrast/demo.svelte';
   import UsePreferredDark from '../core/usePreferredDark/demo.svelte';
   import UsePreferredLanguages from '../core/usePreferredLanguages/demo.svelte';
   import UsePreferredReducedMotion from '../core/usePreferredReducedMotion/demo.svelte';
-  import UseEventListener from '../core/useEventListener/demo.svelte';
-  import UseStorage from '../core/useStorage/demo.svelte';
-  import UseLocalStorage from '../core/useLocalStorage/demo.svelte';
-  import UseSessionStorage from '../core/useSessionStorage/demo.svelte';
+  // Animation
+  import UseTimeoutFn from '../shared/useTimeoutFn/demo.svelte';
+  // Component
+  import UseMounted from '../core/useMounted/demo.svelte';
+  // Utilities
 </script>
 
 <h1 class="mb-6 text-2xl font-bold">Svelte Borya</h1>
 
-<div class="flex flex-col gap-4">
-  <UseClipboardItems />
-  <UseClipboard />
-  <UsePermission />
-  <UseTimeoutFn />
-  <UseBrowserLocation />
-  <UseBreakpoints />
-  <UseEyeDropper />
-  <UseFavicon />
-  <UseDark />
-  <UseColorMode />
-  <UseMediaQuery />
-  <UseMounted />
-  <UsePreferredColorScheme />
-  <UsePreferredContrast />
-  <UsePreferredDark />
-  <UsePreferredLanguages />
-  <UsePreferredReducedMotion />
-  <UseEventListener />
-  <UseStorage />
-  <UseLocalStorage />
-  <UseSessionStorage />
-</div>
+<section>
+  <h2 class="text-3xl font-bold">State</h2>
+  <div class="mt-4 grid gap-4 lg:grid-cols-2">
+    <UseLocalStorage />
+    <UseSessionStorage />
+    <UseStorage />
+  </div>
+</section>
+
+<section class="mt-10">
+  <h2 class="text-3xl font-bold">Browser</h2>
+  <div class="mt-4 grid gap-4 lg:grid-cols-2">
+    <UseClipboardItems />
+    <UseClipboard />
+    <UsePermission />
+    <UseBrowserLocation />
+    <UseBreakpoints />
+    <UseEyeDropper />
+    <UseFavicon />
+    <UseDark />
+    <UseColorMode />
+    <UseMediaQuery />
+    <UsePreferredColorScheme />
+    <UsePreferredContrast />
+    <UsePreferredDark />
+    <UsePreferredLanguages />
+    <UsePreferredReducedMotion />
+    <UseEventListener />
+  </div>
+</section>
+
+<section class="mt-10">
+  <h2 class="text-3xl font-bold">Animation</h2>
+  <div class="mt-4 grid gap-4 lg:grid-cols-2">
+    <UseTimeoutFn />
+  </div>
+</section>
+
+<section class="mt-10">
+  <h2 class="text-3xl font-bold">Component</h2>
+  <div class="mt-4 grid gap-4 lg:grid-cols-2">
+    <UseMounted />
+  </div>
+</section>
+
+<section class="mt-10">
+  <h2 class="text-3xl font-bold">Utilities</h2>
+  <div class="mt-4 grid gap-4 lg:grid-cols-2"><div class="grid gap-4 lg:grid-cols-2"></div></div>
+</section>
