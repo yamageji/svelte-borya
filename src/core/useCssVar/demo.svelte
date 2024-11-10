762ece539d52b418088a40df9e7d0f79b90c89d1
yamageji <koyama.shigehito.chabatake102@gmail.com>
2024-11-11 01:13:45 +0900

    fix: styles

--- src/core/useCssVar/demo.svelte
+++ src/core/useCssVar/demo.svelte
@@ -19,20 +19,37 @@
 </script>
 
 <div class="rounded-md border-2 border-stone-300 p-4">
-  <h2 class="text-lg font-bold">useColorMode</h2>
+  <h2 class="text-lg font-bold">useCssVar</h2>
 
-  <div class="ml-2 mt-2">
-    <div bind:this={el} style="--color: #7fa998; color: var(--color)">
-      Sample text, {cssVar.variable}
+  <div class="ml-2 mt-2 flex flex-col gap-2">
+    <div>
+      <div bind:this={el} style="--color: #7fa998; color: var(--color)">
+        Sample text, {cssVar.variable}
+      </div>
+      <button
+        type="button"
+        onclick={switchColor}
+        class="mt-2 rounded bg-stone-700 px-4 py-2 text-stone-50 duration-200 hover:bg-stone-950 dark:bg-stone-200 dark:text-stone-950 dark:hover:bg-white"
+      >
+        Change Color
+      </button>
     </div>
-    <button onclick={switchColor}> Change Color </button>
-    <div
-      bind:this={elv}
-      style="--color: #7fa998; --color-one: #df8543;"
-      style:color={colorVal.variable}
-    >
-      Sample text, {key}: {colorVal.variable}
+
+    <div>
+      <div
+        bind:this={elv}
+        style="--color: #7fa998; --color-one: #df8543;"
+        style:color={colorVal.variable}
+      >
+        Sample text, {key}: {colorVal.variable}
+      </div>
+      <button
+        type="button"
+        onclick={changeVar}
+        class="mt-2 rounded bg-stone-700 px-4 py-2 text-stone-50 duration-200 hover:bg-stone-950 dark:bg-stone-200 dark:text-stone-950 dark:hover:bg-white"
+      >
+        Change Color Variable
+      </button>
     </div>
-    <button style="margin-left: 0;" onclick={changeVar}> Change Color Variable </button>
   </div>
 </div>
