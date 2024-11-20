baa71905438df9ea51518d0e571d724523b14ae8
yamageji <koyama.shigehito@ceroan.co.jp>
2024-11-20 16:03:57 +0900

    fix: refactor site style

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -49,8 +49,12 @@
 <header
   class="supports-backdrop-blur:bg-neutral-50/95 sticky top-0 flex items-center justify-between gap-6 px-2 py-2"
 >
-  <a href="/" class="rounded-full bg-neutral-50 px-4 py-2 text-2xl font-bold dark:bg-neutral-900"
-    >SvelteBorya</a
+  <a
+    href="/"
+    class="flex items-center gap-1.5 rounded-full bg-neutral-50 px-4 py-2 text-2xl font-bold dark:bg-neutral-900"
+  >
+    <img src="/logo_beta.svg" alt="" class="size-12" />
+    SvelteBorya</a
   >
 
   <nav
